d796059cd9a0c817e1c1fa834110fc30c5698a04
joaomaiavn <129126574+joaomaiavn@users.noreply.github.com>
2024-09-04 20:21:10 -0300

    Commit inicial

---
+++ app.js
@@ -0,0 +1,14 @@
+function sortear() {
+    let quantidade = parseInt(document.getElementById('quantidade').value);
+    let de = parseInt(document.getElementById('de').value);
+    let ate = parseInt(document.getElementById('ate').value);
+
+   
+    let numero = obterNumeroAleatorio(de, ate);
+    alert(numero);
+
+}
+
+function obterNumeroAleatorio(min, max) {
+
+}
